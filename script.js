7a2c1c706ba4ea7be0290495fed77cb9fef9bdf2
Samatar Xasan <Samatar.Xasan@gmail.com>
2020-12-18 18:43:40 +0000

    Icon added for header

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a4c2457465e7636da34177e88b82180e&page=1'
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a4c2457465e7636da34177e88b82180e&page=1";
 
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=a4c2457465e7636da34177e88b82180e&1&query="'
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=a4c2457465e7636da34177e88b82180e&1&query="';
 
-const form = document.getElementById('form')
-const search = document.getElementById('search')
-const main = document.getElementById('main')
-
-
-
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
 
 // window.addEventListener("scroll", scroll);
 
 // function scroll(){
 //     const triggerBottom = (window.innerHeight / 5) * 4;
 //     const boxTop = main.getBoundingClientRect().top;
-    
+
 //     getMovies(API_URL+2)
 
 // }
 
 // Get initial movies
-getMovies(API_URL)
-
+getMovies(API_URL);
 
 async function getMovies(url) {
-    const res = await fetch(url);
-    const data = await res.json();
-    showMovies(data.results)
+  const res = await fetch(url);
+  const data = await res.json();
+  showMovies(data.results);
 }
 
-function showMovies (movies) {
-    main.innerHTML = ""
+function showMovies(movies) {
+  main.innerHTML = "";
 
-    movies.forEach((movie) => {
-        const { title, poster_path, vote_average, overview } = movie
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
 
-        const movieEl = document.createElement('div')
-        movieEl.classList.add('movie')
-        movieEl.innerHTML = `
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
         
                 <img src="${nullImage(poster_path)}"
                 alt="${title}">
@@ -50,36 +48,39 @@ function showMovies (movies) {
             <h3>Overview</h3>
             ${overview}
             </div>
-        `
+        `;
 
-        main.appendChild(movieEl)
-    })
+    main.appendChild(movieEl);
+  });
 }
 
 function getClassbyRate(vote) {
-    if (vote >= 8) {
-        return 'green'
-    } else if (vote >= 5) {
-        return 'orange'
-    } else { return 'red'}
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
 }
 
 function nullImage(path) {
-    if (path !== null) {
-        return IMG_PATH + path
-    } else {return 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png'}
+  if (path !== null) {
+    return IMG_PATH + path;
+  } else {
+    return "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png";
+  }
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const searchTerm = search.value;
+  const searchTerm = search.value;
 
-    if(searchTerm && searchTerm !== '') {
-        getMovies(SEARCH_API + searchTerm)
-        search.value = ''
-    } else {
-        window.location.reload();
-    }
-})
-
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
